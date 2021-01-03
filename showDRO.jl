40cb26de920b9fc1ec0836cf76a2eb75513030c1
Zaki A <zaki@live.ca>
2021-01-02 21:22:39 -0600

    Simplify control cells. Retain slider state

--- showDRO.jl
+++ showDRO.jl
@@ -22,6 +22,38 @@ begin
 	gr(size=(600,400), html_output_format=:png)
 end
 
+# ╔═╡ 38393f20-4d54-11eb-0248-e3937edde1f7
+md"""
+# Fits on Digital Reference Object (DRO)
+
+This document contains three figures.
+
+1. Parameter maps for a model chosen from the list below.
+  + If multiple models are chosen, then the first selected model will be shown.
+  + The parameter map can be changed using the dropdown list above the figure
+
+2. The fitted curve for each chosen model
+  + The chosen voxel is shown by the crosshairs in the parameter map
+  + The sliders will move the crosshairs
+
+3. The residual sum of squares (static figure)
+
+### Choose which models to show
+
+The models represent:
+
+- `exchange`: Two compartment exchange model (2CXM)
+  + Parameters: Fp, PS, ve, vp, T, Te, Tp
+- `extendedtofts`: Extended Tofts model
+  + Parameters: Kt, ve, vp, kep
+- `uptake`: Compartmental tissue uptake model (CTUM)
+  + Parameters: Fp, PS, vp
+- `tofts`: Standed Tofts Model
+  + Parameters: Kt, ve, vp
+
+Multiple models can be selected by holding ctrl while clicking.
+"""
+
 # ╔═╡ 55f56d58-4d64-11eb-3c16-f56d0e192f6c
 md"""
 ### Residual Sum of Squares
@@ -53,39 +85,8 @@ mat = matread("dro.mat");
 # ╔═╡ 98c1115e-4d51-11eb-2880-65657a732856
 allmodels = collect(keys(mat["fits"]))
 
-# ╔═╡ 38393f20-4d54-11eb-0248-e3937edde1f7
-md"""
-# Fits on Digital Reference Object (DRO)
-
-This document contains three figures.
-
-1. Parameter maps for a model chosen from the list below.
-  + If multiple models are chosen, then the first selected model will be shown.
-  + The parameter map can be changed using the dropdown list below the figure
-
-2. The fitted curve for each chosen model
-  + The chosen voxel is shown by the crosshairs in the parameter map
-  + The sliders will move the crosshairs
-
-3. The residual sum of squares (static figure)
-
-### Choose which models to show
-
-The models represent:
-
-- `exchange`: Two compartment exchange model (2CXM)
-  + Parameters: Fp, PS, ve, vp, T, Te, Tp
-- `extendedtofts`: Extended Tofts model
-  + Parameters: Kt, ve, vp, kep
-- `uptake`: Compartmental tissue uptake model (CTUM)
-  + Parameters: Fp, PS, vp
-- `tofts`: Standed Tofts Model
-  + Parameters: Kt, ve, vp
-
-Multiple models can be selected by holding ctrl while clicking.
-
-$(@bind models MultiSelect(allmodels, default = allmodels))
-"""
+# ╔═╡ a9dcc76e-4d72-11eb-04eb-715f93e63d8e
+@bind models MultiSelect(allmodels, default = allmodels)
 
 # ╔═╡ e395e576-4d63-11eb-1ca4-556bafe55491
 let
@@ -113,37 +114,14 @@ ct = mat["ct"];
 # ╔═╡ caf170f4-4d53-11eb-1a6a-bb3fd836acdc
 (midx, midy, midt) = round.(Int, [nx, ny, nt] ./ 2)
 
-# ╔═╡ 1572b524-4d52-11eb-22a7-09978ae54280
-fits = mat["fits"];
-
-# ╔═╡ bb48c55a-4d51-11eb-2e44-db95a10f4e7a
-allparams = collect(keys(fits[first(models)]))
-
 # ╔═╡ 8a07502c-4d53-11eb-0acc-7bba93ebb256
 md"""
-$(@bind param Select(allparams, default = last(allparams)))
 $(@bind y Slider(3:ny-3; default = midy))
 $(@bind x Slider(3:nx-3; default = midx))
 """
 
-# ╔═╡ 514a525c-4d50-11eb-07c6-97593cc5aec5
-let
-	model = first(models)
-	map = copy(fits[model][param])
-	cmax = quantile(filter(!isnan, map), 0.9)
-	
-	map[x-3:x-2, y] .= 0
-    map[x+2:x+3, y] .= 0
-    map[x, y-3:y-2] .= 0
-    map[x, y+2:y+3] .= 0
-	
-	heatmap(map; yflip = true, clims = (0, cmax), title = "$model: $param")
-end
-
-# ╔═╡ b180e12a-4d5f-11eb-1636-5939375ec6dd
-md"""
-Fitted values at voxel (x = $x, y = $y)
-"""
+# ╔═╡ 1572b524-4d52-11eb-22a7-09978ae54280
+fits = mat["fits"];
 
 # ╔═╡ 9c775c6a-4d54-11eb-2f23-d3b183c95a4f
 let
@@ -164,11 +142,32 @@ let
 	p
 end
 
+# ╔═╡ bb48c55a-4d51-11eb-2e44-db95a10f4e7a
+allparams = collect(keys(fits[first(models)]))
+
+# ╔═╡ 8154514a-4d72-11eb-1649-63c5f44d8978
+@bind param Select(allparams, default = last(allparams))
+
+# ╔═╡ 514a525c-4d50-11eb-07c6-97593cc5aec5
+let
+	model = first(models)
+	map = copy(fits[model][param])
+	cmax = quantile(filter(!isnan, map), 0.9)
+	
+	map[x-3:x-2, y] .= 0
+    map[x+2:x+3, y] .= 0
+    map[x, y-3:y-2] .= 0
+    map[x, y+2:y+3] .= 0
+	
+	heatmap(map; yflip = true, clims = (0, cmax), title = "$model: $param")
+end
+
 # ╔═╡ Cell order:
 # ╟─38393f20-4d54-11eb-0248-e3937edde1f7
+# ╟─a9dcc76e-4d72-11eb-04eb-715f93e63d8e
+# ╟─8154514a-4d72-11eb-1649-63c5f44d8978
 # ╟─514a525c-4d50-11eb-07c6-97593cc5aec5
 # ╟─8a07502c-4d53-11eb-0acc-7bba93ebb256
-# ╟─b180e12a-4d5f-11eb-1636-5939375ec6dd
 # ╟─9c775c6a-4d54-11eb-2f23-d3b183c95a4f
 # ╟─55f56d58-4d64-11eb-3c16-f56d0e192f6c
 # ╟─e395e576-4d63-11eb-1ca4-556bafe55491
